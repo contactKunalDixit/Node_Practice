11d965eb598fbc46e76caeb1b549de5fe7bb8cf7
Kunal Dixit <contactkunaldixit@gmail.com>
2021-09-26 14:09:35 +0800

    Updatedd from Linux

---
+++ Node_Tutorials/Notes.jsx
@@ -0,0 +1,28 @@
+/*
+
+Q: What is a NodeJs?
+A:  It is an environment to run JS outside of Browser.
+    Helps at backend to provide server services and database connecting services and more to frontEnd of applications
+    Built on Chrome's V8 JS engine
+
+
+Q: What is the difference between Browser Vs Node.JS ?
+A: 
+ Browser:
+    We have access and work with DOM
+    There is a Window Object in Browser which is native and comprises of various inbuilt methods and properties
+    You can interact with applications through frontEnd functionality in Browser.
+    There is No Filesystems to work with
+    Fragmentations instead of Version
+    ES6 Modules is used. e.g. import {<module>} from "../."  
+
+
+NodeJS:
+    Since the entire implementation happens in Terminal, hence there is no need for reaching browser. Hence, no DOM exists in NodeJS
+    There uis no Window Object since window object comes from Browser and there in no Browser services that exists in NodeJS
+    NODEJS in primarily used for Server Side Apps and services
+    There is FileSystems structure that exsists
+    Versions instead of Fragmentations
+    CommonJS is used instead of ES6 modules e.g. const <moduleName> = require("./..")
+
+*/
